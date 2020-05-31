e02f13232288e906696c99ab5f40e8a51c141468
Avery Martin <checkmate032@gmail.com>
2020-05-31 23:05:40 +0700

    Init

--- app/src/main/java/com/example/applicationidea/activities/MainActivity.java
+++ app/src/main/java/com/example/applicationidea/activities/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.applicationidea.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.applicationidea.R;
+import com.example.applicationidea.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginFragment = new LoginFragment();
+
+        loadFragment(loginFragment);
+    }
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().replace(R.id.frameLayoutMain, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/applicationidea/fragments/LoginFragment.java
+++ app/src/main/java/com/example/applicationidea/fragments/LoginFragment.java
@@ -7,10 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.applicationidea.R;
 
 public class LoginFragment extends Fragment {
+    private View mView;
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonSubmit;
+    private TextView textViewNewUser;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -19,6 +26,12 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        mView  =  inflater.inflate(R.layout.fragment_login, container, false);
+
+        editTextEmail = mView.findViewById(R.id.editTextEmail);
+        editTextPassword = mView.findViewById(R.id.editTextPassword);
+        buttonSubmit = mView.findViewById(R.id.buttonSubmitMain);
+
+        return mView;
     }
 }
