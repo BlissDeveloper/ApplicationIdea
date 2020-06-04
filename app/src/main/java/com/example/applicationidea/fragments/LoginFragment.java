a3347ca183b170e99eba76bdacb648c32be3f80b
Avery Martin <checkmate032@gmail.com>
2020-06-04 23:09:39 +0700

    Init

--- app/src/main/java/com/example/applicationidea/fragments/LoginFragment.java
+++ app/src/main/java/com/example/applicationidea/fragments/LoginFragment.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import com.example.applicationidea.R;
 
+
 public class LoginFragment extends Fragment {
     private View mView;
     private EditText editTextEmail, editTextPassword;
