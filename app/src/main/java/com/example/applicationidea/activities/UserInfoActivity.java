4ddf334bb75cf2b3f011143e36899e6ee4d6fc6e
Avery Martin <checkmate032@gmail.com>
2020-06-13 19:39:59 +0700

    Added UserInfoActivity

--- app/src/main/java/com/example/applicationidea/activities/UserInfoActivity.java
+++ app/src/main/java/com/example/applicationidea/activities/UserInfoActivity.java
@@ -1,16 +1,44 @@
 package com.example.applicationidea.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.example.applicationidea.R;
 
 public class UserInfoActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    private EditText editTextFirstName, editTextMiddleName, editTextLastName, editTextDOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        initViews();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void initViews() {
+        toolbar = findViewById(R.id.toolbarUserInfo);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.tell_us_about));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
